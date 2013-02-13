aad8e08bbb34deadaa0a1a10f7fcfa1034bdcd12
Matthew Gordon <ratherironic@gmail.com>
2013-02-13 02:20:44 -0700

    adding initial static portfolio site files.

---
+++ http-async.js
@@ -0,0 +1,42 @@
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path');
+
+http.createServer(function(req, res) {
+	res.setHeader('Content-Type', 'text/html');
+	var html = '';
+
+	var filePath = '.' + req.url;
+	if (filePath == './') {
+		filePath = './index.html';
+	}
+
+	var contentType = 'text/html';
+    var extname = path.extname(filePath);
+    switch (extname) {
+    	case '.jpg':
+    		contentType = 'image/jpeg';
+    		break;
+    	case '.png':
+    		contentType = 'image/png';
+    		break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    fs.readFile(filePath, function(err, data) {
+    	if(err) {
+    		res.writeHead(500); 
+            res.end();
+    	} else {
+		    res.writeHead(200, { 'Content-Type': contentType });
+		    res.end(data, 'utf-8');
+    	}
+    });
+}).listen(8080, function() {
+	console.log('Listening on 8080....');
+})
